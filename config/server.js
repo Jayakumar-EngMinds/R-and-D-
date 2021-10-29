decd104b26b61c5937ff9f87a4bd6b83a90e8613
Viacheslav Antonenko <viacheslav.antonenko@uinno.io>
2021-10-29 14:59:19 +0300

    initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'f81ef6fd1133877c68f58feb8d7d64ef'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'b92cbac5-829d-438f-8e38-88f11a483daf'
+};
