157b9e74fa08d4061f02a178f278c6b333a71311
Viacheslav Antonenko <viacheslav.antonenko@uinno.io>
2021-11-01 15:06:09 +0200

    feature: added footer, connect DB

--- config/database.js
+++ config/database.js
@@ -1,11 +1,16 @@
 module.exports = ({ env }) => ({
-  defaultConnection: 'default',
+  defaultConnection: "default",
   connections: {
     default: {
-      connector: 'bookshelf',
+      connector: "bookshelf",
       settings: {
-        client: 'sqlite',
-        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+        client: "postgres",
+        host: env("DATABASE_HOST", "postgres"),
+        port: env.int("DATABASE_PORT", 5432),
+        database: env("DATABASE_NAME", ""),
+        username: env("DATABASE_USERNAME", ""),
+        password: env("DATABASE_PASSWORD", ""),
+        ssl: env.bool("DATABASE_SSL", false),
       },
       options: {
         useNullAsDefault: true,
