6918ba1c18037bf3d85240b4596710ec604ebbd1
Viacheslav Antonenko <viacheslav.antonenko@uinno.io>
2022-11-15 20:23:41 +0200

    feat: add service/solution categories/pages

---
+++ api/service-pages/models/service-pages.js
@@ -0,0 +1,22 @@
+"use strict";
+const { showError } = require("../../../utils/notification");
+
+/**
+ * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#lifecycle-hooks)
+ * to customize this model
+ */
+
+module.exports = {
+  lifecycles: {
+    async beforeCreate(data) {
+      if (!data.service_category) {
+        showError("Please select category", 422);
+      }
+    },
+    async beforeUpdate(_, data) {
+      if (!("published_at" in data) && !data.service_category) {
+        showError("Please select category", 422);
+      }
+    },
+  },
+};
