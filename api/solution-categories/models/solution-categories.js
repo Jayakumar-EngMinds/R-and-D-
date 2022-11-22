968ed0c106758b564e3b4ada8db4a90bd28b8295
Viacheslav Antonenko <viacheslav.antonenko@uinno.io>
2022-11-22 16:06:40 +0200

    feat: add guard for categories to have at least one page;

--- api/solution-categories/models/solution-categories.js
+++ api/solution-categories/models/solution-categories.js
@@ -1,8 +1,22 @@
-'use strict';
+"use strict";
+const { showError } = require("../../../utils/notification");
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#lifecycle-hooks)
  * to customize this model
  */
 
-module.exports = {};
+module.exports = {
+  lifecycles: {
+    async beforeCreate(data) {
+      if (!data.solution_pages) {
+        showError("Please select category page(s)", 422);
+      }
+    },
+    async beforeUpdate(_, data) {
+      if (!("published_at" in data) && !data.solution_pages) {
+        showError("Please select category page(s)", 422);
+      }
+    },
+  },
+};
